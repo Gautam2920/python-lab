d569d6f06a2bffed5f9378091974d49e5e896fbe
Gautam2920 <gautambhargava2004@gmail.com>
2026-02-10 11:18:32 +0530

    code updated

--- day04/replaceVowels.py
+++ day04/replaceVowels.py
@@ -1,1 +1,12 @@
 # WAP to replace all vowels (lowecase and uppercase) with * in a string.
+
+import re
+
+string1 = input("Enter your string: ")
+
+vowels = "aeiouAEIOU"
+replace = "*"
+
+result = re.sub(r'[aeiouAEIOU]', replace , string1)
+
+print(result)
